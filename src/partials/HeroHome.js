116ec06a1699ae7c87215db291ea0cbea3598c8f
elbica <sh0705k@gmail.com>
2021-11-03 11:03:18 +0900

    init

---
+++ src/partials/HeroHome.js
@@ -0,0 +1,183 @@
+import React, { useState } from "react";
+import Modal from "../utils/Modal";
+
+function HeroHome() {
+  const [videoModalOpen, setVideoModalOpen] = useState(false);
+
+  return (
+    <section className="relative">
+      {/* Illustration behind hero content */}
+      <div
+        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
+        aria-hidden="true"
+      >
+        <svg
+          width="1360"
+          height="578"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01"
+            >
+              <stop stopColor="#FFF" offset="0%" />
+              <stop stopColor="#EAEAEA" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
+        </svg>
+      </div>
+
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        {/* Hero content */}
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          {/* Section header */}
+          <div className="text-center pb-12 md:pb-16">
+            <h1
+              className="text-5xl md:text-6xl text-gray-600 font-extrabold leading-tighter tracking-tighter mb-4"
+              data-aos="zoom-y-out"
+            >
+              요리는 이제
+              <br />
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-carrot-100 to-citrus-100">
+                Cheffi 와 함께!
+              </span>
+            </h1>
+            <div className="max-w-3xl mx-auto">
+              <p
+                className="text-xl text-gray-600 mb-8"
+                data-aos="zoom-y-out"
+                data-aos-delay="150"
+              >
+                그동안 레시피를 찾는 일이 번거롭지 않으셨나요? <br />
+                내가 가진 재료로 좋아하는 레시피를 추천 받아 보세요 🎉
+              </p>
+              <div
+                className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
+                data-aos="zoom-y-out"
+                data-aos-delay="300"
+              >
+                <div>
+                  <a
+                    className="btn text-white bg-carrot-100 hover:bg-carrot-200 w-full mb-4 sm:w-auto sm:mb-0"
+                    href="#0"
+                  >
+                    Start free trial
+                  </a>
+                </div>
+                <div>
+                  <a
+                    className="btn text-white bg-vegetable-100 hover:bg-vegetable-200 w-full sm:w-auto sm:ml-4"
+                    href="#0"
+                  >
+                    Learn more
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Hero image */}
+          <div>
+            <div
+              className="relative flex justify-center mb-8"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay="300"
+            >
+              <div className="flex flex-col justify-center">
+                <img
+                  className="mx-auto"
+                  src={require("../images/hero-image.png").default}
+                  width="768"
+                  height="432"
+                  alt="Hero"
+                />
+                <svg
+                  className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto"
+                  width="768"
+                  height="432"
+                  viewBox="0 0 768 432"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                >
+                  <defs>
+                    <linearGradient
+                      x1="50%"
+                      y1="0%"
+                      x2="50%"
+                      y2="100%"
+                      id="hero-ill-a"
+                    >
+                      <stop stopColor="#FFF" offset="0%" />
+                      <stop stopColor="#EAEAEA" offset="77.402%" />
+                      <stop stopColor="#DFDFDF" offset="100%" />
+                    </linearGradient>
+                    <linearGradient
+                      x1="50%"
+                      y1="0%"
+                      x2="50%"
+                      y2="99.24%"
+                      id="hero-ill-b"
+                    >
+                      <stop stopColor="#FFF" offset="0%" />
+                      <stop stopColor="#EAEAEA" offset="48.57%" />
+                      <stop stopColor="#DFDFDF" stopOpacity="0" offset="100%" />
+                    </linearGradient>
+                  </defs>
+                  <g fill="none" fillRule="evenodd">
+                    <circle
+                      fillOpacity=".04"
+                      fill="url(#hero-ill-a)"
+                      cx="384"
+                      cy="216"
+                      r="128"
+                    />
+                    <circle
+                      fillOpacity=".16"
+                      fill="url(#hero-ill-b)"
+                      cx="384"
+                      cy="216"
+                      r="96"
+                    />
+                    <g fillRule="nonzero">
+                      <use fill="#000" xlinkHref="#hero-ill-d" />
+                      <use fill="url(#hero-ill-e)" xlinkHref="#hero-ill-d" />
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </div>
+
+            <Modal
+              id="modal"
+              ariaLabel="modal-headline"
+              show={videoModalOpen}
+              handleClose={() => setVideoModalOpen(false)}
+            >
+              <div className="relative pb-9/16">
+                <iframe
+                  className="absolute w-full h-full"
+                  src="https://player.vimeo.com/video/174002812"
+                  title="Video"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </Modal>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HeroHome;
